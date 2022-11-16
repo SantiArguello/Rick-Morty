97fedb1041e0d2f39b25c412642e0e1e1675106b
Chorosombrero <Saanti_arguello@hotmail.com>
2022-11-15 21:05:35 -0300

    first commit

---
+++ src/components/Cards.jsx
@@ -0,0 +1,16 @@
+import Card from './Card';
+
+export default function Cards(props) {
+   const { characters } = props;
+   return <div>
+
+      {characters.map((e) => 
+         <Card name={e.name}
+            species={e.species}
+            gender={e.gender}
+            image={e.image}
+            key={e.name}
+               />)}
+
+   </div>;
+}
