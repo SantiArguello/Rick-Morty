a71ea02f513bfd2f80744dccec9fd3538dd3908d
Chorosombrero <Saanti_arguello@hotmail.com>
2022-11-22 17:34:50 -0300

    correccion responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import SearchBar from "./SearchBar.jsx";
 
+
 export default function Nav(props) {
     return (
 
-        <nav> <SearchBar onSearch={props.onSearch} /> </nav>
+        <nav  > <SearchBar onSearch={props.onSearch} /> </nav>
 
     )
 
